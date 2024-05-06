68fc023234e07aee901f69dab2bb8d2017951a7d
tgirma11 <girmalegesseterfa@gmail.com>
2024-05-06 15:22:29 -0700

    updated the image and relate with name

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,26 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const selectElement = document.getElementById("horn-select");
+
+  // Event listener for horn selection change
+  selectElement.addEventListener('change', (event) => {
+    const selectedHorn = event.target.value;
+    const imageElement = document.querySelector("main img");
+    
+    // Update image source based on selected horn
+    switch (selectedHorn) {
+      case "air-horn":
+        imageElement.src = "assets/images/air-horn.svg";
+        break;
+      case "car-horn":
+        imageElement.src = "assets/images/car-horn.svg";
+        break;
+      case "party-horn":
+        imageElement.src = "assets/images/party-horn.svg";
+        break;
+      default:
+        break;
+    }
+  });
 }
